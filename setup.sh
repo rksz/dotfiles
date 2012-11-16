c6699530f95482dd3f3e576024fb819956d53c4a
Ricky <shimma.dev@gmail.com>
2012-11-16 10:38:46 +0900

    added auto vim plugin install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+#
+# auto setup tools
+#
+#########################################
 
+
+#========================================
+# create symbolic links
+#========================================
 DOT_FILES=( .zsh .zshrc .zshrc.alias .ctags .gitconfig .gitignore .vimrc .vrapperrc .tmux.conf .dir_colors .vim )
 
 for file in ${DOT_FILES[@]}
@@ -7,7 +15,17 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+
+#========================================
+# Import submodule
+#========================================
 git submodule update --init
 
-# final step:
-echo "Run vim & :BundleInstall"
+
+
+#========================================
+# Install vim plugins
+#========================================
+vim +BundleInstall +qall
+
+
