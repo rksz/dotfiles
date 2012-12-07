995af5a6c348693a29937ec46746bbc1da310d08
Ricky <shimma.dev@gmail.com>
2012-12-07 12:37:04 +0900

    - Added: vimdiff wrapper script config.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #
-# auto setup tools
+# auto setup tools @rskz
 #
 #########################################
 
 
-#========================================
-# create symbolic links
-#========================================
+## create symbolic links
 DOT_FILES=( .zsh .zshrc .zshrc.alias .ctags .gitconfig .gitignore .vimrc .vrapperrc .tmux.conf .dir_colors .vim )
 
 for file in ${DOT_FILES[@]}
@@ -16,16 +14,22 @@ do
 done
 
 
-#========================================
-# Import submodule
-#========================================
+## Import submodule
 git submodule update --init
 
 
-
-#========================================
-# Install vim plugins
-#========================================
-vim +NeoBundleInstall +qall
+## Install vim plugins
+vim +NeoBundleInstall
 
 
+## Install git diff with vimdiff
+cat <<EOC >>$HOME/dotfiles/.gitconfig
+[diff]
+  external = git_diff_wrapper
+[pager]
+  diff =
+EOC
+
+
+## finished!
+echo "finished!"
