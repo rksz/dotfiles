0f4ccfd5249326e44f9d0c7a784250db36e27793
Ricky <master@mistdev.com>
2013-11-21 12:17:17 +0900

    UPDATE setup script - adds os specific settings (osx, linux) - refactors code

--- setup.sh
+++ setup.sh
@@ -4,6 +4,64 @@
 #
 #########################################
 
+
+DOT_FILES=(
+.zsh
+.zshrc
+.zshrc.alias
+.ctags
+.gitignore
+.vimrc
+.tmux.conf
+.dir_colors
+.vim
+.gvimrc
+)
+
+LOCAL_FILES=(
+.zshrc.alias.local
+.tmux.conf.local
+)
+
+BREW_PLUGINS=(
+vim
+tmux
+tig
+reattach-to-user-namespace
+autojump
+fuse4x
+sshfs
+htop
+ack
+ctags
+git-flow
+tree
+watch
+wget
+curl
+mosh
+nkf
+openssl
+lynx
+xz
+binutils
+coreutils
+findutils
+sl
+)
+
+YUM_PLUGINS=(
+tmux
+tig
+tree
+fuse-sshfs
+htop
+ack
+dstat
+mosh
+)
+
+
 main() {
 
     #Definition
@@ -11,30 +69,14 @@ main() {
     readonly CMDNAME=$(basename $0)
 
     ## 1. Create symbolic links
-    DOT_FILES=(
-    .zsh
-    .zshrc
-    .zshrc.alias
-    .ctags
-    .gitignore
-    .vimrc
-    .vrapperrc
-    .tmux.conf
-    .dir_colors
-    .vim
-    .gvimrc
-    )
     for file in ${DOT_FILES[@]}
     do
         ln -s $HOME/dotfiles/$file $HOME/$file
     done
-    exit
+    echo "[setup notice] If you don't use zshell as login shell, run following command."
+    echo "- sudo cp -a $HOME/dotfiles/bin/git_diff_wrapper /usr/local/bin/"
 
     ## 2. Create local configure files
-    LOCAL_FILES=(
-    .zshrc.alias.local
-    .tmux.conf.local
-    )
     for file in ${LOCAL_FILES[@]}
     do
         touch $HOME/$file
@@ -44,15 +86,37 @@ main() {
     git submodule update --init
 
     ## 4. Install vim plugins
-    vim +NeoBundleInstall
+    vim +NeoBundleInstall! 2>/dev/null
 
-    ## 5. Copy gitdiff wrapper
-    sudo cp -a $HOME/dotfiles/bin/git_diff_wrapper /usr/local/bin/
+    ## 5. Specific settings
+    case "${OSTYPE}" in
 
-    ## 6. Finished messages
-    echo "Finished! Install following packages (brew or epel)"
-    echo "brew install vim tmux tig reattach-to-user-namespace"
+        # Mac(Unix)
+        darwin*)
+            brew install ${BREW_PLUGINS[@]}
+            brew update
+
+            #for sshfs
+            sudo /bin/cp -rfX /usr/local/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions
+            sudo chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x
+
+            echo "[setup notice] DOWNLOAD FOLLOWING APPLICATIONS MANUALLY"
+            echo "- MacVim: https://code.google.com/p/macvim-kaoriya/downloads/list"
+            echo "- Vagrant: http://downloads.vagrantup.com/"
+            echo "- VirtualBox: https://www.virtualbox.org/wiki/Downloads"
+        ;;
+        # Linux
+        linux*)
+            rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+            yum -y install ${YUM_PLUGINS[@]}
+        ;;
+
+    esac
+
+    echo "============================================================"
+    echo "------------------ SETUP WAS COMPLETED! --------------------"
+    echo "============================================================"
+
 }
 
 main "$@"
-
