1922bb1b033b560c1e8bc530860d9e7c9639abde
Ricky <master@mistdev.com>
2014-01-29 23:24:39 +0900

    updates shell/bash skelton file

--- .vim/skeleton/skeleton.sh
+++ .vim/skeleton/skeleton.sh
@@ -5,28 +5,21 @@
 #########################################
 
 BASEPATH=$(cd `dirname $0`; pwd)
-CMDNAME=$(basename $0)
-
 
 main() {
 
-    #Validation
-    if [[ $# -lt 1 ]]; then
+    log "INFO" "START"
         set -x
-        echo "usage: $CMDNAME" 1>&2
         set +x
-        exit 1
-    fi
-
-    #Main
 
 }
 
-logging() {
+log() {
     now=$(date "+%Y-%m-%d %H:%M:%S")
     mode=$1
     msg=$2
     echo -e "\e[33m ${now} [${mode}]: ${msg} \e[m"
+    # echo "${now} [${mode}]: ${msg}"
 }
 
 main "$@"
